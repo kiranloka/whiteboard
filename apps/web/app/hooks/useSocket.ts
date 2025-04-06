068dc010407eb5c2b24bdd022264a0d502595e43
kiranLoka <kirankumarloka@gmail.com>
2025-04-06 20:35:25 +0530

    initial frontend and chatroom logic

---
+++ apps/web/app/hooks/useSocket.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+const WS_URL = "http://localhost:8080";
+
+export function useSocket() {
+  const [loading, setLoading] = useState(true);
+  const [socket, setSocket] = useState<WebSocket>();
+
+  useEffect(() => {
+    const ws = new WebSocket(
+      `${WS_URL}?eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxNDMzOTdjMy01OTNkLTQwMjctYjExNC0yOTAyNGJhYjAyMTgiLCJpYXQiOjE3MzY2OTczMzB9.BxDMP3FqBsM6TrZcAGYFRA2FlmazFwQJ78mOHskatiM`
+    );
+    ws.onopen = () => {
+      setLoading(false);
+      setSocket(ws);
+    };
+  }, []);
+
+  return {
+    socket,
+    loading,
+  };
+}
---
+++ apps/web/app/room/[slug]/page.tsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+async function getRoomId(slug: string) {
+  const response = await axios.get("${BACKEND_URL}/room/${slug}");
+  console.log(response.data);
+  return response.data.room.id;
+}
+
+export default async function ChatRoom1({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const slug = (await params).slug;
+  const roomId = await getRoomId(slug);
+
+  return <ChatRoom id={roomId}></ChatRoom>;
+}
